cf99754fd08775f36a1a035028c95d553d0f0dc1
RKSTUDIOGAMES <beginnerbuild@gmail.com>
2026-03-21 19:17:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -815,4 +815,5 @@ def admin_logout():
     return redirect("/admin_login")
 init_db()
 if __name__ =="__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
